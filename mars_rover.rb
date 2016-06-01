3352024eee767b6d6b9e680ec5afb6add6786bcc
spencer <spencer.dossett@gmail.com>
2016-06-01 13:56:21 -0400

    promt for and clean input for plateau_size

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,2 +1,9 @@
 # x(-1 to 1) = East to West
 # y(-1 to 1) = South to North
+
+
+puts "Please input plateau size in x,y integer format:"
+user_input = gets.chomp
+plateau_size = user_input.partition(/.,/)
+
+plateau_size.map! {|coordinate|  coordinate.tr(",","")}
