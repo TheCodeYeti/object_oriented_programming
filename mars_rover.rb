338a0261520056c56cc1822a1c0a0908fc102671
spencer <spencer.dossett@gmail.com>
2016-06-01 15:45:34 -0400

    Made MissionControl run everything

--- mars_rover.rb
+++ mars_rover.rb
@@ -4,11 +4,20 @@
 def clean_input(user_input)
   if user_input.include? ","
     cleaned_input = user_input.split(",")
-  else
+  elsif user_input.include? " "
     cleaned_input = user_input.split(" ")
   end
   cleaned_input
 end
+
+def can_be_int?(input)
+  if !!/\A\d+\z/.match(input)
+    return true
+  else
+    return false
+  end
+end
+
 
 class Plateau
   attr_accessor(:x, :y)
@@ -19,8 +28,9 @@ class Plateau
 end
 
 class Rover
-  attr_reader(:x, :y, :direction)
-  def initialize(coordinates) # expecting array of x,y,direction
+  attr_reader(:x, :y, :direction, :name)
+  def initialize(coordinates, name) # expecting array of x,y,direction
+    @name = name
     @x = coordinates[0]
     @y = coordinates[1]
     @direction = coordinates[2]
@@ -45,14 +55,45 @@ class Rover
 end
 
 class MissionControl
-  def initialize()
+  attr_accessor :rover_count
+  def initialize(plateau_coordinates)
+    @mars = Plateau.new(plateau_coordinates)
+
+    puts "How many rovers are on Mars? "
+    rover_count = gets.chomp.to_i
+    @rovers = []
+    rover_count.times do |rover_num|
+      puts "Give me rover##{rover_num + 1}'s name: "
+      rover_name = gets.chomp
+      puts "Give me #{rover_name}'s coordinates in x,y,d format where x and y are the coordinates and d is the direction #{rover_name} is facing (N,S,E,W): "
+      rover_coordinates = gets.chomp
+      rover = Rover.new(clean_input(rover_coordinates), rover_name)
+      @rovers << rover
+    end
   end
+
+  def list_rovers
+    puts "There are #{@rovers.count} rovers on the Matrian Plateau"
+    @rovers.each do |rover|
+      puts "#{rover.name}=> X:#{rover.x} Y:#{rover.y} Facing:#{rover.direction}"
+    end
+  end
+
+  def send_instructions(rover, instructions)
+  end
+
 end
 
-puts "Please input Martian plateau size in x,y integer format: "
+puts "Come in Mission Control... Initializing connection..."
+puts "Mission Control, Please input Martian plateau size in x,y integer format: "
 user_input = gets.chomp
-mars = Plateau.new(clean_input(user_input)) # create martian plateau
+houston = MissionControl.new(clean_input(user_input))
+houston.list_rovers
+#
+# user_input = gets.chomp
+# mars = Plateau.new(clean_input(user_input)) # create martian plateau
+#
 
-puts "How many Rovers are on Mars? "
-user_input = gets.chomp
-pathfinder = Rover.new(clean_input(user_input))
+
+
+# pathfinder = Rover.new(clean_input(user_input))
