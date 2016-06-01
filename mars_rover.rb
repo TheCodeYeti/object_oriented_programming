b84e06915e8984f095fa9a541da11b67360464b1
spencer <spencer.dossett@gmail.com>
2016-06-01 14:40:09 -0400

    created read_instruction, move, and turn methods for the Rover class

--- mars_rover.rb
+++ mars_rover.rb
@@ -11,6 +11,7 @@ def clean_input(user_input)
 end
 
 class Plateau
+  attr_accessor(:x, :y)
   def initialize(coordinates) # expecting array of x,y
     @x = coordinates[0]
     @y = coordinates[1]
@@ -18,10 +19,28 @@ class Plateau
 end
 
 class Rover
+  attr_reader(:x, :y, :direction)
   def initialize(coordinates) # expecting array of x,y,direction
     @x = coordinates[0]
     @y = coordinates[1]
     @direction = coordinates[2]
+  end
+  def read_instruction(instruction)
+    case instruction.upcase
+      when "L", "R" then turn(instruction.upcase)
+      when "M" then move(instruction.upcase)
+    end
+  end
+  def move
+    case @direction
+      when "N" then @y += 1
+      when "S" then @y -= 1
+      when "E" then @x -= 1
+      when "W" then @x += 1
+    end
+  end
+  def turn
+
   end
 end
 
@@ -31,3 +50,5 @@ cleaned_input = clean_input(user_input)
 
 # plateau_size.map! {|coordinate|  coordinate.tr(", ","")}
 mars = Plateau.new(cleaned_input)
+
+pathfinder = Rover.new(cleaned_input)
