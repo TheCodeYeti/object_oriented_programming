ded486f5cfa67aab2dd88f02668aa2be661983bc
spencer <spencer.dossett@gmail.com>
2016-06-01 14:10:08 -0400

    made the clean process a method and improved it

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,9 +1,20 @@
 # x(-1 to 1) = East to West
 # y(-1 to 1) = South to North
 
+def clean_input(user_input)
+  if user_input.include? ","
+    cleaned_input = user_input.split(",")
+  else
+    cleaned_input = user_input.split(" ")
+  end
+  cleaned_input
+end
+
+
 
 puts "Please input plateau size in x,y integer format:"
 user_input = gets.chomp
-plateau_size = user_input.partition(/.,/)
+cleaned_input = clean_input(user_input)
 
-plateau_size.map! {|coordinate|  coordinate.tr(",","")}
+# plateau_size.map! {|coordinate|  coordinate.tr(", ","")}
+puts plateau_size
